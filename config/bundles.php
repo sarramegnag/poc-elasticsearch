7a8d66e9e1e425d902d7fe4d3689142dc3ce5070
Guillaume SARRAMEGNA <guillaume.sarramegna@sensiolabs.com>
2023-01-18 19:10:12 +0100

    Switch books index to elasticsearch

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
-use App\Repository\BookRepository;
+use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
+    private const MAX_ITEMS_PER_PAGE = 10;
+
     #[Route('/', name: 'app_book')]
-    public function index(BookRepository $bookRepository): Response
-    {
+    public function index(
+        #[Autowire(service: 'fos_elastica.finder.book')]
+        PaginatedFinderInterface $bookFinder,
+        Request $request
+    ): Response {
+        $page = $request->query->get('page', 1);
+
+        $books = $bookFinder->findPaginated([])
+            ->setMaxPerPage(self::MAX_ITEMS_PER_PAGE)
+            ->setCurrentPage($page);
+
         return $this->render('book/index.html.twig', [
-            'books' => $bookRepository->findAll(),
+            'books' => $books,
         ]);
     }
 }
